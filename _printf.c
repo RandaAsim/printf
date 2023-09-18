0a91f38a2f1c27079f7243ebd243ea7f198cd71f
RandaAsim <randasanfora2015@gmail.com>
2023-09-18 16:47:54 -0700

    second chance

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _printf - Outputs a formatted string
+ * @format: Character string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *out;
+	va_list args;
+	int t;
+
+	if (format == NULL)
+		return (-1);
+	out = init_buffer();
+	if (out == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	t = format_printf(format, args, out);
+
+	return (t);
+}
---
+++ format_printf.c
@@ -0,0 +1,46 @@
+/**
+ * format_printf - the format string for _printf
+ * @format: charcter
+ * @out: buffer
+ * @args: va_list of arguments
+ * Return: number
+ */
+int format_printf(const char *format, va_list args, buffer_r *out)
+{
+	int i, w, prec, t = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_r *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = h_flags(format + i + 1, &tmp);
+			w = h_width(args, format + i + tmp + 1, &tmp);
+			prec = h_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = h_length(format + i + tmp + 1, &tmp);
+
+			f = h_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				t += f(args, out, flags, w, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				t = -1;
+				break;
+			}
+		}
+		t += _memcpy(out, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanall(args, out);
+	return (t);
+}
